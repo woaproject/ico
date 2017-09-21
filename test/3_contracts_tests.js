8df1f29bc506e33a03eac6dda88a87534597aa02
viktor <baranov.viktor.27@gmail.com>
2017-09-20 17:54:59 -0700

    typo

--- test/3_contracts_tests.js
+++ test/3_contracts_tests.js
@@ -4,7 +4,7 @@ var FlatPricingExt = artifacts.require("./FlatPricingExt.sol");
 var MintedTokenCappedCrowdsaleExt = artifacts.require("./MintedTokenCappedCrowdsaleExt.sol");
 
 contract('SafeMathLibExt', function(accounts) {
-	it("should put multiply 2 to 3", function() {
+	it("should multiply 2 to 3", function() {
 	    return SafeMathLibExt.deployed().then(function(instance) {
 	      return instance.times.call(2,3);
 	    }).then(function(res) {
