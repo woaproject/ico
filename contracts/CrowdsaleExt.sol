db06c06123fac071ba0cf9a761c43dbeb422ba44
viktor <baranov.viktor.27@gmail.com>
2017-10-26 19:34:26 +0300

    isBreakingCap fix

--- contracts/CrowdsaleExt.sol
+++ contracts/CrowdsaleExt.sol
@@ -248,11 +248,6 @@ contract CrowdsaleExt is Haltable {
       }
     }
 
-    // Check that we did not bust the cap
-    if(isBreakingCap(weiAmount, tokenAmount, weiRaised, tokensSold)) {
-      throw;
-    }
-
     if(investedAmountOf[receiver] == 0) {
        // A new investor
        investorCount++;
@@ -268,6 +263,11 @@ contract CrowdsaleExt is Haltable {
 
     if(pricingStrategy.isPresalePurchase(receiver)) {
         presaleWeiRaised = presaleWeiRaised.plus(weiAmount);
+    }
+
+    // Check that we did not bust the cap
+    if(isBreakingCap(weiAmount, tokenAmount, weiRaised, tokensSold)) {
+      throw;
     }
 
     assignTokens(receiver, tokenAmount);
